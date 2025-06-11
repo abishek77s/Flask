59f189be12e6816ec5a1069165678182d640f06d
abishek77s <abishek22004@gmail.com>
2025-06-11 15:50:16 +0530

    getFile() handles the file opening now, Added UpdateJSON() to handle updation of the json file.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import os
 
-file_path = "E:/SkillRank/JSON/ex5.json"
+file_path = "E:/Flask/ex5.json"
 
 app = Flask(__name__)
 
-@app.route("/api/hello", methods=['GET'])
-def hello():
-    return "Hello, World!"
 
-
-@app.route("/api/json", methods=['GET'])
-def getJSON():
+def getFile():
     try:
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"File not found: {file_path}")
@@ -27,5 +22,50 @@ def getJSON():
     except FileNotFoundError as fnf_error:
         return jsonify({"error": "File not found"}), 404
 
+
+@app.route("/api/hello", methods=['GET'])
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/api/json", methods=['GET'])
+def getJSON():
+    ex5 = getFile()
+    return ex5
+    
+    
+@app.route("/api/json", methods=['PUT'])
+def updateJSON():
+    data = request.json
+    name = data.get('name')
+    id = data.get('id')
+    type = data.get('type')
+
+    try:
+        ex5 = getFile()
+    except FileNotFoundError:
+        return ex5
+    
+    for donut in ex5:
+        found = False
+        if donut.get("name") == name:
+            found = True
+            batter = donut.get("batters", {}).get("batter", [])
+            
+            if not any(b.get("id") == id for b in batter):
+                batter.append({
+                    "id": str(id),
+                    "type": type
+                })
+                with open(file_path, 'w') as file:
+                    json.dump(ex5, file, indent=2)
+                return jsonify({"Success": f"Added batter into {name}"}), 200
+            else:
+                return jsonify({"error": f"Batter with id:{id} already exists."}), 409
+    
+    if not found:
+            return jsonify({"error": f"Donut named {name} not found."}), 400
+
+    
 if __name__ == "__main__":
     app.run(debug=True)

b5d6f895a1af6b0341ad64aa55b065ceedc34a16
abishek77s <abishek22004@gmail.com>
2025-06-12 21:22:48 +0530

    Now you can create new JSON files using createJSON(), also moved the updating, creating files into a separate function, endpoint now has the filename for GET and PUT methods.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request
 import json
 import os
 
-file_path = "E:/Flask/ex5.json"
+
 
 app = Flask(__name__)
 
 
-def getFile():
+def GetFile(filename):
+    file_path = "E:/Flask/"+ filename + ".json"
     try:
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"File not found: {file_path}")
@@ -21,6 +22,18 @@ def getFile():
         
     except FileNotFoundError as fnf_error:
         return jsonify({"error": "File not found"}), 404
+    
+def UpdateFile(filename, content):
+    try:
+        with open("E:/Flask/"+ filename + ".json", 'w') as file:
+            json.dump(content, file, indent=2)
+
+    except FileNotFoundError as fnf_error:
+        return jsonify({"error": "Failed to create new file."}), 404
+            
+       
+                
+
 
 
 @app.route("/api/hello", methods=['GET'])
@@ -28,25 +41,38 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/api/json", methods=['GET'])
-def getJSON():
-    ex5 = getFile()
-    return ex5
+@app.route("/api/json/<filename>", methods=['GET'])
+def getJSON(filename):
+    file = GetFile(filename)
+    return file
+
+
+@app.route("/api/json/create", methods=['POST'])
+def createJSON():
+    data = request.json
+    filename = data.get('filename')
+    content = data.get('content')
+    try:
+        if os.path.exists("E:/Flask/"+ filename + ".json"):
+            return jsonify({"Failed": f"File {filename} already exists."}), 409
+        status = UpdateFile(filename, content)
+        return jsonify({"Success": f"File {filename} has been created."}), 200
+    except FileNotFoundError:
+        return status
+
     
-    
-@app.route("/api/json", methods=['PUT'])
-def updateJSON():
+@app.route("/api/json/<filename>", methods=['PUT'])
+def updateJSON(filename):
     data = request.json
     name = data.get('name')
     id = data.get('id')
     type = data.get('type')
-
     try:
-        ex5 = getFile()
+        JSONfile = GetFile(filename)
     except FileNotFoundError:
-        return ex5
+        return JSONfile
     
-    for donut in ex5:
+    for donut in JSONfile:
         found = False
         if donut.get("name") == name:
             found = True
@@ -57,12 +83,10 @@ def updateJSON():
                     "id": str(id),
                     "type": type
                 })
-                with open(file_path, 'w') as file:
-                    json.dump(ex5, file, indent=2)
+                UpdateFile(filename, JSONfile)
                 return jsonify({"Success": f"Added batter into {name}"}), 200
             else:
                 return jsonify({"error": f"Batter with id:{id} already exists."}), 409
-    
     if not found:
             return jsonify({"error": f"Donut named {name} not found."}), 400
 
